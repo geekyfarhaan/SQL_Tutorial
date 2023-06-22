6c315b7723c11e9b814ec4832fafa546327f789d
geekyfarhaan <geekyfarhaan@gmail.com>
2023-06-23 00:15:45 +0530

    First Commit

---
+++ learnsql.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE trendytech;
+SHOW DATABASES;
+DROP DATABASE trendytech;
+SHOW DATABASES;
+USE trendytech;
+SELECT database();
+CREATE TABLE employee
+(
+name VARCHAR(50),
+age INT,
+salary INT
+);
+
+SHOW TABLES;
+
+DESC employee;
+
+drop table employee;
+
+show tables;
+
+select database();
+
+drop DATABASE trendytech;
+
+SELECT database();
+
+CREATE TABLE trendytech.employee
+(
+name VARCHAR(50),
+age INT,
+salary INT
+);
+
+select database();
+
+use trendytech;
+
+show tables;
+
+SELECT database();
+
